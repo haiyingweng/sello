ea5a4aaee37158183f67b67921306088f0b09812
Haiying Weng <hw534@cornell.edu>
2020-05-08 00:54:02 -0400

    Add product and category models

--- src/dao.py
+++ src/dao.py
@@ -1,1 +1,40 @@
-from db import db
+from db import db, Product, Category
+
+
+def get_all_products():
+    return [p.serialize() for p in Product.query.all()]
+
+
+def create_product(name, description, condition, price):
+    new_product = Product(
+        name=name
+        description=description
+        condition=condition
+        price=price
+    )
+    db.session.add(new_product)
+    db.session.commit()
+    return new_product.serialize()
+
+
+def get_product_by_id(id):
+    product = Product.query.filter_by(id=id).first()
+    if product is None:
+        return None
+    return product.serialize()
+
+
+def delete_product_by_id(id):
+    product = Product.query.filter_by(id=id).first()
+    if product is None:
+        return None
+    db.session.delete(product)
+    db.session.commit()
+    return product.serialize()
+
+
+def get_category_by_id(id):
+    category = Category.query.filter_by(id=id).first()
+    if Category is None:
+        return None
+    return category.serialize()
--- src/db.py
+++ src/db.py
@@ -1,3 +1,41 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    condition = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', '')
+        self.description = kwargs.get('description', '')
+        self.condition = kwargs.get('condition', '')
+        self.price = kwargs.get('price', '')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'condition': self.condition,
+            'price': self.price
+        }
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.category = kwargs.get('category', '')
+
+    def serialize(self):
+        return {
+            'category': self.category
+        }
